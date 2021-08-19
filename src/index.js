bd4c298b687bdd759719f8dee81ad5a648dbdb3a
hoshico <77495217+hoshico@users.noreply.github.com>
2021-08-20 08:33:21 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -22,20 +22,27 @@
 
 // const val3 = "const変数を再宣言";
 
-
 //constで定義したオブジェクトや配列はプロパティの変更が可能
 const val4 = {
   name: "やす",
-  age: 34,
+  age: 34
 };
 val4.name = "yasuyuki";
-val4.address = "Osaka"; 
+val4.address = "Osaka";
 console.log(val4);
 
-const val5 = ['dog', 'cat'];
+const val5 = ["dog", "cat"];
 val5[0] = "bird";
 val5.push("monkey");
 console.log(val5);
 
+//テンプレート文字列
+const name = "やす";
+const age = 34;
 
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
 
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
